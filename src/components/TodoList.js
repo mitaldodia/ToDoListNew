5b150e6adb5a5d04de575b402500e8a836729ae1
mitaldodia <mdodia@kernshell.com>
2022-12-01 16:36:34 +0530

    clr button

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -40,14 +40,19 @@ function TodoList() {
     setTodos(updatedTodos);
   };
 
+// clear cart function 
+const handleCartClearance = () =>{
+  setTodos([]);
+}
+
   return (
     <>
       <h1>What's the Plan for Today?</h1>
       <div className="from-brn-detail">
         <TodoForm onSubmit={addTodo} />
-        {/* <div className="clr-btn-det">
+        <div className="clr-btn-det">
           <button className='clr-btn' onClick={handleCartClearance}>Clear All</button>
-        </div> */}
+        </div>
       </div>
       <Todo
         todos={todos}
